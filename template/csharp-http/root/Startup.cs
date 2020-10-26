ed3ca4bd5252fe0e2596267198814c44c45481e5
Goncalo Oliveira <goncalo.oliveira@fonixtelematics.com>
2020-10-26 16:50:30 +0100

    removed unused references

--- template/csharp-http/root/Startup.cs
+++ template/csharp-http/root/Startup.cs
@@ -2,15 +2,11 @@ using System;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
-using Redpanda.OpenFaaS;
 using root;
 
 namespace template
