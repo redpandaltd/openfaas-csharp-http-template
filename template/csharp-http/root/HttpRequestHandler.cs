d9c70555041f3855f5746e6b65ec9e3d5793f875
Goncalo Oliveira <goncalo.oliveira@fonixtelematics.com>
2020-10-26 14:53:50 +0100

    some refactoring on the request handler

---
+++ template/csharp-http/root/HttpRequestHandler.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Redpanda.OpenFaaS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace root
+{
+    internal class HttpRequestHandler
+    {
+        private readonly IHttpFunction function;
+        private readonly ILogger log;
+
+        public HttpRequestHandler( ILoggerFactory loggerFactory, IHttpFunction httpFunction )
+        {
+            log = loggerFactory.CreateLogger<HttpRequestHandler>();
+            function = httpFunction;
+        }
+
+        public async Task HandleAsync( HttpContext context )
+        {
+            if ( context.Request.Path != "/" )
+            {
+                context.Response.StatusCode = 404;
+
+                await context.Response.WriteAsync( "Not Found" );
+
+                log.LogInformation( $"{context.Request.Method} {context.Request.Path}  404" );
+
+                return;
+            }
+
+            try
+            {
+                // get function http modifiers
+                var methodInfo = function.GetType().GetMethod( "HandleAsync" );
+                var httpAttributes = methodInfo.GetCustomAttributes( typeof( Microsoft.AspNetCore.Mvc.Routing.HttpMethodAttribute ), false );
+
+                if ( httpAttributes.Any() )
+                {
+                    // we have http modifiers, let's match with the request method
+                    var attributeName = $"http{context.Request.Method}attribute";
+
+                    if ( !httpAttributes.Any( x => x.GetType().Name.Equals( attributeName, StringComparison.OrdinalIgnoreCase ) ) )
+                    {
+                        // method not allowed
+                        context.Response.StatusCode = 405;
+
+                        await context.Response.WriteAsync( "Method Not Allowed" );
+                        return;
+                    }
+                }
+
+                // execute function
+                var result = await function.HandleAsync( context.Request );
+
+                // write result
+                var actionContext = new ActionContext( context, new RouteData(), new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor() );
+
+                await result.ExecuteResultAsync( actionContext );
+            }
+            catch ( NotImplementedException ex )
+            {
+                context.Response.StatusCode = 501;
+
+                await context.Response.WriteAsync( ex.ToString() );
+
+                log.LogError( ex, ex.Message );
+            }
+            catch ( Exception ex )
+            {
+                context.Response.StatusCode = 500;
+
+                await context.Response.WriteAsync( ex.ToString() );
+
+                log.LogError( ex, ex.Message );
+            }
+        }
+    }
+}
--- template/csharp-http/root/Startup.cs
+++ template/csharp-http/root/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using Redpanda.OpenFaaS;
+using root;
 
 namespace template
 {
@@ -49,6 +50,9 @@ namespace template
 
             // allow function implementation to add services to the container
             new OpenFaaS.Startup().ConfigureServices( services );
+
+            // add root request handler to the container
+            services.AddTransient<HttpRequestHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,59 +63,9 @@ namespace template
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run( async context =>
-            {
-                if ( context.Request.Path != "/" )
-                {
-                    context.Response.StatusCode = 404;
+            var requestHandler = app.ApplicationServices.GetRequiredService<HttpRequestHandler>();
 
-                    await context.Response.WriteAsync( "404" );
-                    return;
-                }
-
-                try
-                {
-                    var function = app.ApplicationServices.GetService<IHttpFunction>();
-
-                    // get function http modifiers
-                    var methodInfo = typeof( OpenFaaS.Function ).GetMethod( "HandleAsync" );
-                    var httpAttributes = methodInfo.GetCustomAttributes( typeof( Microsoft.AspNetCore.Mvc.Routing.HttpMethodAttribute ), false );
-
-                    if ( httpAttributes.Any() )
-                    {
-                        // we have http modifiers, let's match with the request method
-                        var attributeName = $"http{context.Request.Method}attribute";
-
-                        if ( !httpAttributes.Any( x => x.GetType().Name.Equals( attributeName, StringComparison.OrdinalIgnoreCase ) ) )
-                        {
-                            // method not allowed
-                            context.Response.StatusCode = 405;
-
-                            await context.Response.WriteAsync( "405" );
-                            return;
-                        }
-                    }
-
-                    // execute function
-                    var result = await function.HandleAsync( context.Request );
-
-                    // write result
-                    var actionContext = new ActionContext( context, new RouteData(), new Microsoft.AspNetCore.Mvc.Abstractions.ActionDescriptor() );
-
-                    await result.ExecuteResultAsync( actionContext );
-                }
-                catch ( NotImplementedException ex )
-                {
-                    context.Response.StatusCode = 501;
-                    await context.Response.WriteAsync( ex.ToString() );
-                }
-                catch ( Exception ex )
-                {
-                    context.Response.StatusCode = 500;
-                    await context.Response.WriteAsync( ex.ToString() );
-                }
-
-            } );
+            app.Run( requestHandler.HandleAsync );
         }
     }
 }
